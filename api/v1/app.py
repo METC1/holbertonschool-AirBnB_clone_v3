dfafaf2ca6e9a86f2f9d6271e5ec25e726d215ea
METC <metc_ck1@hotmail.com>
2022-09-20 10:52:14 -0700

    api status

---
+++ api/v1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""RESTful API AirBnB Clone"""
+from models import storage
+from api.vi.views import app_views
+from flask import Flask, jsonify, make_response
+from os import getenv
+from flask_cors import CORS
+
+
+app= Flask(__name__)
+cors = CORS(app, resources={r"/*":{"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def storage_close(self):
+    """handles @app.teardown_appcontext which calls storage.close()"""
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 errors not found and returns a JSON - format 404"""
+    return jsonify({'error': 'Not found'}),404
+
+if __name__ == '__main__':
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=getenv('HBNB_API_PORT', '5000'), threaded=True)
+
+
+
---
+++ api/v1/views/index.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" app_views routes"""
+from api.v1.views import app_views
+from flask import Flask, jsonify
+from models import storage
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """route /status returns a status:OK in the JSON format"""
+    return jsonify({'status': 'ok'})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """route /stats returns the number of objects by type"""
+    return jsonify({"amenities": storage.count('Amenity'),
+                    "cities": storage.count('City'),
+                    "places": storage.count('Place'),
+                    "reviews": storage.count('Review'),
+                    "states": storage.count('State'),
+                    "users": storage.count('User')})
