1efb066b43d3c084b2adb50e75f22f5dc7229ae2
METC <metc_ck1@hotmail.com>
2022-09-20 21:00:28 -0700

    refactor index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
 """
-manages app views routes using flask and python
-also uses jsonify to convert to json
+index.py file returns status and stats of API
 """
 
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import jsonify
+# import json
 from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status')
 def status():
     """
-    route status returns a status OK in the JSON format
+    Returns status as a flask response
     """
-    return (jsonify({'status': 'ok'}))
+    python_dict = {"status": "OK"}
+    # The jsonify() returns a flask.Response() object that already has the
+    # appropriate content-type header ‘application/json’ for use with json
+    # responses. Whereas, the json.dumps() method will just return an encoded
+    # string, which would require manually adding the MIME type header.
+    # json_string = json.dumps(python_dict)
+    flask_response = jsonify(python_dict)
+    return flask_response
 
 
-@app_views.route('/stats', strict_slashes=False)
+@app_views.route('/stats')
 def stats():
     """
-    route stats returns the number of objects by type
+    Returns the number of all objects by type class as a flask response
     """
-    return (jsonify({"amenities": storage.count('Amenity'),
-                     "cities": storage.count('City'),
-                     "places": storage.count('Place'),
-                     "reviews": storage.count('Review'),
-                     "states": storage.count('State'),
-                     "users": storage.count('User')}))
+    python_dict = {"amenities": storage.count("Amenity"), "cities": storage.
+                   count("City"), "places": storage.count("Place"), "reviews":
+                   storage.count("Review"), "states": storage.count("State"),
+                   "users": storage.count("User")}
+    flask_response = jsonify(python_dict)
+    return flask_response
