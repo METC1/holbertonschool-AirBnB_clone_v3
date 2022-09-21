f75f7ac009ddc877adc6ccc773ad7dea02c9a8bc
METC <metc_ck1@hotmail.com>
2022-09-20 21:02:27 -0700

    Refactor api v1 views __init__

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """
-create a variable app_views which is an instance of Blueprint
+__init__.py file
 """
 
 from flask import Blueprint
 
 
-app_views = Blueprint('api', __name__, url_prefix='/api/v1',)
-
-from api.v1.views.states import *
+app_views = Blueprint(
+    'api',
+    __name__,
+    url_prefix='/api/v1',
+)
 from api.v1.views.index import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
+from api.v1.views.states import *
